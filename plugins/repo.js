0e7a5ad30a3a6a403686414fa1b834d3f7372fa3
Caseyrhodes tech <boogy6391@gmail.com>
2025-08-24 22:04:27 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴛᴇᴄʜ

--- plugins/repo.js
+++ plugins/repo.js
@@ -72,7 +72,7 @@ ${readMore}
             message: {
                 contactMessage: {
                     displayName: "CASEYRHODES VERIFIED ✅",
-                    vcard: "BEGIN:VCARD\nVERSION:3.0\nFN: Caseyrhodes VERIFIED ✅\nORG:CASEYRHODES-TECH BOT;\nTEL;type=CELL;type=VOICE;waid=254112192119:+254112192119\nEND:VCARD"
+                    vcard: "BEGIN:VCARD\nVERSION:3.0\nFN: Caseyrhodes VERIFIED ✅\nORG:CASEYRHODES-TECH BOT;\nTEL;type=CELL;type=VOICE;waid=13135550002:+13135550002\nEND:VCARD"
                 }
             }
         };
